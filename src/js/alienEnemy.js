c1e0e51088b1260425fa31d3031dbe31d3bd64af
chaimCeres <91317303+chaimCeres@users.noreply.github.com>
2025-06-01 01:11:40 +0200

    collison aangepast van alien (2)levens en mine (3) levens

--- src/js/alienEnemy.js
+++ src/js/alienEnemy.js
@@ -1,8 +1,21 @@
 import { Enemy } from './enemy.js';
 import { Resources } from './resources.js';
+import { Player } from './player.js';
 
 export class AlienEnemy extends Enemy {
     constructor(x, y) {
         super(x, y, 100, 100, Resources.Alien.toSprite(), -320);
+    }
+
+    onInitialize(engine) {
+        this.on('collisionstart', (event) => {
+            const other = event.other?.owner;
+            if (other instanceof Player) {
+                // 3 levens eraf
+                if (other.game && typeof other.game.loseLife === 'function') {
+                    other.game.loseLife(2);
+                }
+            }
+        });
     }
 }
--- src/js/mineEnemy.js
+++ src/js/mineEnemy.js
@@ -1,8 +1,26 @@
 import { Enemy } from './enemy.js';
 import { Resources } from './resources.js';
+import { Player } from './player.js';
 
 export class MineEnemy extends Enemy {
     constructor(x, y) {
         super(x, y, 60, 60, Resources.Mine.toSprite(), -100);
+    }
+
+    onInitialize(engine) {
+        this.on('collisionstart', (event) => {
+            const other = event.other?.owner;
+            if (other instanceof Player) {
+                // 2 levens eraf
+                if (other.game && typeof other.game.loseLife === 'function') {
+                    other.game.loseLife(3);
+                }
+                // Optioneel: snelheid verlagen
+                other.vel.x *= 0.5;
+                setTimeout(() => {
+                    other.vel.x *= 2;
+                }, 2000);
+            }
+        });
     }
 }
